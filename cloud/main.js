f094e811d429a9f52a8a673558b9695315b56d5b
Takeshi YAEDA <t.yaeda@gmail.com>
2014-08-09 00:29:10 +0900

    Add crawl function

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,25 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+var crawl = function (url, success, error) {
+  Parse.Cloud.httpRequest({
+    url: url,
+    success: function (httpResponse) {
+      success();
+    },
+    error: function (httpResponse) {
+      error();
+    }
+  });
+};
+
+Parse.Cloud.define('crawl', function(request, response) {
+  crawl(request.params.url,
+        function () {
+          response.success('success');
+        },
+        function () {
+          console.err('Request failed with response code ' + httpRequest.status);
+        });
+});
